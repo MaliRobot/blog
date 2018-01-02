b701ee38289d6e5dd033c13a2be975b3fa1ad409
Milos Trifunovic <milos.trifunovic@gmail.com>
2018-01-02 21:01:11 +0100

    more models

--- nonlinear/flow/models.py
+++ nonlinear/flow/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+from datetime import datetime
 
 # Create your models here.
 class Image(models.Model):
@@ -13,6 +14,32 @@ class Post(models.Model):
     text = models.TextField()
     author = models.OneToOneField(User)
     public = models.BooleanField()
+    language = models.CharField(max_length=3, default='eng')
     images = models.ForeignKey(Image, on_delete=models.CASCADE)
     date_published = models.DateTimeField('date published')
+
+class Comment(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    author = models.CharField(max_length=255)
+    public = models.BooleanField(default=True)
+    date_posted = models.DateTimeField('date posted', blank=False)
+    post = models.ForeignKey(Post, related_name='comments')
+
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    description = models.TextField()
+    code = models.CharField(max_length=12)
+    release_date = models.DateTimeField('release date')
+    images = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+class Order(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    package = models.TextField()
+    date_ordered = models.DateTimeField('date ordered', blank=False)
+
+
+
 
