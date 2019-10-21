40d0ec99b1cb4d723ec022755ff4f6b58c3e9c91
MaliRobot <milos.trifunovic@gmail.com>
2019-10-21 22:35:46 +0200

    fix nonlinear settings

--- nonlinear/nonlinear/settings.py
+++ nonlinear/nonlinear/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 DEBUG_TOOLBAR_PANELS = [
