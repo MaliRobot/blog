0ca32dbcb9c11baf0445fc029bcd0885a0b19718
MaliRobot <milos.trifunovic@gmail.com>
2020-10-13 03:29:34 +0200

    ie and ff fixes for sidemenu

--- nonlinear/flow/static/JS/functions.js
+++ nonlinear/flow/static/JS/functions.js
@@ -21,6 +21,23 @@ $(window).on('load', function() {
     });
 });
 
-$(window).load(function() {
-  $("body").removeClass("preload");
-});
+$(document).ready(function (){
+    function msieversion()
+    {
+        var ua = window.navigator.userAgent;
+        var msie = ua.indexOf("MSIE ");
+
+        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    if (msieversion())
+    {
+        console.log('as');
+        $('#menu-links').removeClass('col-md-2');
+    }
+})
