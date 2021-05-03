27c57a13bab2d714f0d1a06ce575b9de3203d97b
MaliRobot <milos.trifunovic@gmail.com>
2021-05-03 20:31:20 +0200

    styles and js

---
+++ nonliteral/core/static/JS/functions.js
@@ -0,0 +1,42 @@
+$(window).on('load', function() {
+    var color = 0;
+    $('.invisible_box').on('click', function(e) {
+        let colors = [
+            'aliceblue',
+            'antiquewhite',
+            'azure',
+            'beige',
+            'cornsilk',
+            'ghostwhite',
+            'honeydew',
+            'papayawhip'
+        ];
+        if (color > colors.length - 1) {
+            color = 0;
+        }
+        console.log(color);
+        $(this).css('background-color', colors[color]);
+        $('body').css('background-color', colors[color]);
+        color++;
+    });
+});
+
+$(document).ready(function (){
+    function msieversion()
+    {
+        var ua = window.navigator.userAgent;
+        var msie = ua.indexOf("MSIE ");
+
+        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    if (msieversion())
+    {
+        $('#menu-links').removeClass('col-md-2');
+    }
+})
