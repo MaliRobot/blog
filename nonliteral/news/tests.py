228c500cf3b4f9b5f366de47d8293aaeb95420f4
MaliRobot <milos.trifunovic@gmail.com>
2023-02-09 00:10:44 +0100

    add basic news test

--- nonliteral/news/tests.py
+++ nonliteral/news/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from news.models import News
+
+import datetime
+
 
 # Create your tests here.
+class NewsTestCase(TestCase):
+    def setUp(self):
+        News.objects.create(
+            title="test1",
+            text="lorem ipsum",
+            public=0,
+            language='eng',
+            image="/test",
+            date_published=datetime.date.today()
+        )
+
+    def test_news_read(self):
+        news = News.objects.get(title="test1")
+        self.assertEqual(news.text, "lorem ipsum")
